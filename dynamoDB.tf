d84ecffd067905c1e92f78b4054bdb373ef31cdd
root <root@DESKTOP-S7GGDE6.localdomain>
2021-06-05 17:35:52 -0300

    Start project

---
+++ dynamoDB.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "cloudz-dynamodb-table" {
+    name = "KemuelSongs"
+    billing_mode = "PAY_PER_REQUEST"
+    read_capacity = 10 
+    write_capacity = 10 
+    hash_key = "Artist"
+    range_key = "Song Title"
+
+    attribute { 
+        name = "Artist"
+        type = "S"
+    }
+
+    attribute { 
+        name = "Song Title"
+        type = "S"
+    }
+ tags = { 
+     Name = "dynamodb-table1"
+     Environment = "production"
+ }
+
+}
